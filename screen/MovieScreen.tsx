6b421c27890fd318b9c53730cd088109539f7128
Ebuka Umeh <ebukaumeh@Ebukas-MacBook-Pro.local>
2020-08-13 11:11:54 +0100

    api

--- screen/MovieScreen.tsx
+++ screen/MovieScreen.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text } from 'react-native'
 import { HomeComponent } from '../components'
+import { MovieTypes } from '../helper/Types'
+import { requestMovieScreen as RequestMovieApi } from '../api/api'
 
 export default function MovieScreen() {
+    const [moviesData, setMoviesData] = useState([])
+
+    useEffect(() => {
+        requestMovieScreen()
+    },[])
+
+    const requestMovieScreen = async () => {
+        await RequestMovieApi((data) => setMoviesData(data))
+    }
+
+    console.log('moviesData', moviesData)
+    
     return (
         <HomeComponent />
     )
