c11f7c79a508bc50afa4fdb0a8235d50ffd34001
Ebuka Umeh <ebukaumeh@Ebukas-MacBook-Pro.local>
2020-08-13 07:58:58 +0100

    navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Navigator from './navigation'
+import { useFonts } from 'expo-font'
+console.disableYellowBox = true
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    "Montserrat-Bold": require('./assets/fonts/Montserrat-Bold.ttf'),
+    "Montserrat-Light": require('./assets/fonts/Montserrat-Light.ttf'),
+    "Montserrat-Medium": require('./assets/fonts/Montserrat-Medium.ttf'),
+    "Montserrat-Regular": require('./assets/fonts/Montserrat-Regular.ttf'),
+    "Montserrat-SemiBold": require('./assets/fonts/Montserrat-SemiBold.ttf')
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Navigator />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +28,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
 });
---
+++ navigation/index.tsx
@@ -0,0 +1,74 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { Text } from 'react-native'
+import { black, white, orange } from '../helper/Color'
+import MovieScreen from '../screen/MovieScreen'
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack'
+
+const Drawer = createDrawerNavigator()
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+    return (
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerTitle: false,
+          headerTransparent: true,
+          headerBackTitleVisible: false,
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeDrawerNavigator} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    );
+  };
+
+const HomeDrawerNavigator = () => {
+    return (
+      <Drawer.Navigator
+        initialRouteName="Movies"
+        drawerType={"slide"}
+        drawerStyle={{ width: "50%", backgroundColor: black }}
+        drawerContentOptions={{
+          activeBackgroundColor: "transparent",
+          activeTintColor: orange,
+          inactiveTintColor: white,
+        }}
+      >
+        <Drawer.Screen
+          name="Movies"
+          component={MovieScreen}
+          options={{
+            drawerLabel: ({ color, focused }) => CustomDrawerStyle(color, focused, "Movies"),
+          }}
+        />
+      </Drawer.Navigator>
+    );
+  };
+
+  const CustomDrawerStyle = (color, focused, title) => {
+    return (
+      <Text
+        style={{
+          fontSize: focused ? 20 : 16,
+          fontWeight: null,
+          color: color,
+          fontFamily: focused ? "Montserrat-Bold" : "Montserrat-Light",
+        }}
+      >
+        {title}
+      </Text>
+    );
+  };
+
+
+export default function index() {
+  return (
+    <NavigationContainer>
+        <AppNavigator />
+    </NavigationContainer>
+  );
+}
